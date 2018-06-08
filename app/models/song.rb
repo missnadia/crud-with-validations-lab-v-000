b63f8175fcc30116271cfefb516f14f46081b791
Nadia Lee <miss.nadia.lee@gmail.com>
2018-06-08 19:09:27 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,14 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
   validates :released, inclusion: { in: [ true, false ] }
-  validates :release_year
   validates :artist_name, presence: true
+
+  with_options if: :released? do |r|
+    r.validates :release_year, presence: true
+    r.validates :release_year, numericality: { only_integer: true, less_than_or_equal_to: Date.today.year }
+  end
+  
+  def released?
+    released
+  end
 end
