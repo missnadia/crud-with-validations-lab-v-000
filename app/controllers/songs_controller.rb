864b742e2b956d5f3284a3a0a56e1468aa7e58c9
Nadia Lee <miss.nadia.lee@gmail.com>
2018-06-08 19:14:37 +0000

    automatically backed up by learn

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,22 @@
+class SongsController < ApplicationController
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def create
+    @song = Song.new(song_params)
+    if @song.save
+      redirect_to song_path(@song)
+end
